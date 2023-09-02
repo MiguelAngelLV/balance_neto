6e13d68d45f2ebea8f4f7f8b32084d00cab0b3d3
Mguel Ángel <miguel.angel.lopez.vicente@gmail.com>
2023-09-02 14:48:23 +0200

    Multicuenta y loggings

--- custom_components/balance_neto/__init__.py
+++ custom_components/balance_neto/__init__.py
@@ -1,9 +1,13 @@
 """Balance Neto"""
 from __future__ import annotations
-
+import logging
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
-from homeassistant.core import HomeAssistant
+from homeassistant.core import HomeAssistant, callback
+from homeassistant.helpers.entity_registry import async_migrate_entries
+from homeassistant.helpers import entity_registry as er
+
+_LOGGER = logging.getLogger(__name__)
 
 from .const import DOMAIN
 
@@ -17,4 +21,28 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    return True
+
+
+async def async_migrate_entry(hass: HomeAssistant, config_entry):
+    version = config_entry.version
+
+    if version == 1:
+        @callback
+        def _async_migrator(entity_entry: er.RegistryEntry):
+            new_unique_id = entity_entry.unique_id
+            old_unique_id = entity_entry.unique_id
+            if "import" in entity_entry.unique_id:
+                new_unique_id = f"{entity_entry.config_entry_id}-import"
+            if "export" in entity_entry.unique_id:
+                new_unique_id = f"{entity_entry.config_entry_id}-export"
+            if "balance" in entity_entry.unique_id:
+                new_unique_id = f"{entity_entry.config_entry_id}-balance"
+
+            _LOGGER.debug("Updating unique_id from %s to %s", old_unique_id, new_unique_id)
+            return {"new_unique_id": new_unique_id}
+
+        await er.async_migrate_entries(hass, config_entry.entry_id, _async_migrator)
+        config_entry.version = 2
+
     return True
--- custom_components/balance_neto/sensor.py
+++ custom_components/balance_neto/sensor.py
@@ -1,13 +1,9 @@
 from datetime import datetime
 from datetime import timedelta
+import logging
 from typing import Mapping, Any
-import asyncio
-
-from homeassistant.const import EVENT_HOMEASSISTANT_STARTED
-from .const import MAX_DIFF
 
 from homeassistant.components.sensor import (
-    STATE_CLASS_MEASUREMENT,
     STATE_CLASS_TOTAL_INCREASING,
     SensorEntity,
     DEVICE_CLASS_ENERGY,
@@ -15,12 +11,13 @@ from homeassistant.components.sensor import (
     RestoreEntity
 )
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import EVENT_HOMEASSISTANT_STARTED
 from homeassistant.core import HomeAssistant
 from homeassistant.core import State
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.helpers.event import async_track_state_change
-from homeassistant.helpers.typing import StateType
 from homeassistant.helpers.event import async_track_point_in_time
+from homeassistant.helpers.event import async_track_state_change
+from .const import MAX_DIFF
 
 EXPORT_DESCRIPTION = SensorEntityDescription(
     key="exportado_a_la_red",
@@ -50,27 +47,29 @@ BALANCE_DESCRIPTION = SensorEntityDescription(
     suggested_display_precision=2
 )
 
+_LOGGER = logging.getLogger(__name__)
+
 
 async def async_setup_entry(
         hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
 ) -> None:
-    grid_import = GridSensor(IMPORT_DESCRIPTION)
-    grid_export = GridSensor(EXPORT_DESCRIPTION)
-
     import_id = entry.data['grid_import']
     export_id = entry.data['grid_export']
-    grid_balance = BalanceSensor(BALANCE_DESCRIPTION, grid_import, grid_export, import_id, export_id, hass)
+
+    grid_import = GridSensor(IMPORT_DESCRIPTION, f"{entry.entry_id}-import")
+    grid_export = GridSensor(EXPORT_DESCRIPTION, f"{entry.entry_id}-export")
+
+    grid_balance = BalanceSensor(BALANCE_DESCRIPTION, grid_import, grid_export, f"{entry.entry_id}-balance")
 
     async_add_entities([grid_import, grid_export, grid_balance])
 
-    def update_import(changed_entity: str, old_state: State | None, new_state: State | None):
-        grid_balance.update_import(new_state)
+    def update_values(changed_entity: str, old_state: State | None, new_state: State | None):
+        grid_balance.update_values()
 
-    def update_export(changed_entity: str, old_state: State | None, new_state: State | None):
-        grid_balance.update_export(new_state)
+    async_track_state_change(hass, import_id, update_values)
+    async_track_state_change(hass, export_id, update_values)
 
-    async_track_state_change(hass, import_id, update_import)
-    async_track_state_change(hass, export_id, update_export)
+    _LOGGER.debug("Starting Balance Neto with %s for import and %s for export", import_id, export_id)
 
     async def update_totals_and_schedule(now):
         grid_balance.update_totals()
@@ -86,16 +85,17 @@ async def async_setup_entry(
 
 
 class GridSensor(SensorEntity, RestoreEntity):
-    def __init__(self, description: SensorEntityDescription, ) -> None:
+    def __init__(self, description: SensorEntityDescription, unique_id) -> None:
         super().__init__()
         self._state = 0
         self._attrs: Mapping[str, Any] = {}
         self._attr_name = description.name
-        self._attr_unique_id = description.key
+        self._attr_unique_id = unique_id
         self.entity_description = description
         self._reboot = None
 
     async def async_added_to_hass(self):
+        _LOGGER.debug("Added %s", self._attr_unique_id)
         if (last_sensor_data := await self.async_get_last_state()) is not None:
             self._state = float(last_sensor_data.state)
 
@@ -112,6 +112,7 @@ class GridSensor(SensorEntity, RestoreEntity):
     def update_value(self, value: float):
         self._state = float(self._state) + value
         self.async_write_ha_state()
+        _LOGGER.debug("Updating value %f for %s", self._state, self._attr_unique_id)
 
     def after_reboot(self):
         self._reboot = datetime.now().isoformat()
@@ -122,9 +123,7 @@ class BalanceSensor(SensorEntity, RestoreEntity):
     def __init__(self, description: SensorEntityDescription,
                  import_sensor: GridSensor,
                  export_sensor: GridSensor,
-                 import_id: str,
-                 export_id: str,
-                 hass
+                 unique_id
                  ) -> None:
         super().__init__()
         self._import = 0
@@ -137,7 +136,7 @@ class BalanceSensor(SensorEntity, RestoreEntity):
         self._state = 0
         self._attrs: Mapping[str, Any] = {}
         self._attr_name = description.name
-        self._attr_unique_id = description.key
+        self._attr_unique_id = unique_id
         self.entity_description = description
 
         self._last_reset = None
@@ -170,43 +169,39 @@ class BalanceSensor(SensorEntity, RestoreEntity):
 
     def _update_value(self):
         self._state = (self._export - self._export_offset) - (self._import - self._import_offset)
+        _LOGGER.debug("Actual Balance %f", self._state)
         self.async_write_ha_state()
 
-    def update_import(self, state: State | None):
-        if not self._is_value_valid(state):
+    def update_values(self):
+
+        try:
+            import_state = float(self._import_sensor.state)
+            export_state = float(self._export_sensor.state)
+
+            if self._import_offset == 0:
+                self._import_offset = import_state
+
+            diff = abs(import_state - self._import_offset)
+            if diff > MAX_DIFF:
+                self._import_offset = import_state
+
+            if self._export_offset == 0:
+                self._export_offset = export_state
+
+            diff = abs(export_state - self._export_offset)
+            if diff > MAX_DIFF:
+                self._export_offset = export_state
+
+            _LOGGER.warning("Updating Balance Neto. Actual Import %f, Export %f. Import offset %f, Export offset %f",
+                            import_state, export_state, self._import_offset, self._export_offset)
+
+            self._update_value()
+        except ValueError:
             return
-
-        value = float(state.state)
-
-        if self._import_offset == 0:
-            self._import_offset = value
-
-        diff = abs(value - self._import_offset)
-        if diff > MAX_DIFF:
-            self._import_offset = value
-
-        self._import = value
-
-        self._update_value()
-
-    def update_export(self, state: State | None):
-        if not self._is_value_valid(state):
-            return
-
-        value = float(state.state)
-
-        if self._export_offset == 0:
-            self._export_offset = value
-
-        diff = abs(value - self._export_offset)
-        if diff > MAX_DIFF:
-            self._export_offset = value
-
-        self._export = value
-        self._update_value()
 
     def update_totals(self):
         value = float(self._state)
+        _LOGGER.debug("Updating net values. Balance %f")
         self._last_reset = (datetime.utcnow() + timedelta(hours=1)).strftime("%Y-%m-%d %H:00:00")
         self._import_offset = self._import
         self._export_offset = self._export
@@ -222,9 +217,10 @@ class BalanceSensor(SensorEntity, RestoreEntity):
         self._export_offset = 0
 
     @staticmethod
-    def _is_value_valid(state):
+    def _is_value_valid(export_state, import_state):
         try:
-            value = float(state.state)
-            return value > 0
+            float(export_state.state)
+            float(import_state.state)
+            return True
         except ValueError:
             return False
